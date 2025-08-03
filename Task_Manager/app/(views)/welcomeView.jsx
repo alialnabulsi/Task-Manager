429ca79ca68f3a4512a2cf82c8ce530cd8d063fd
alialnabulsi <ali.alnabulsi@std.balamand.edu.lb>
2025-08-03 21:12:38 +0300

    step 1 welcome view

--- Task_Manager/app/(views)/welcomeView.jsx
+++ Task_Manager/app/(views)/welcomeView.jsx
@@ -1,5 +1,35 @@
-import {StyleShheet, Text, View} from 'react-native';
+import {StyleShheet, Text, View, Image, Button, Pressable} from 'react-native';
 
 import colors from '../constants/colors.js';
 import sizings from '../constants/sizings.js';
 
+import welcomeImage from '../assets/images/icon.png';
+
+const WelcomeView = () => {
+  return (
+    <View style={styles.container}>
+      <Image source ={welcomeImage}></Image>
+      <Text style={styles.h1Text}>Task Manager</Text>
+      <Text style={styles.h2Text}>Organize your day!</Text>
+      <Pressable style={styles.button}></Pressable>
+    </View>
+  );
+}
+const styles = StyleShheet.create({
+    container:{
+
+    },
+    welcomeImage:{
+
+    },
+    h1Text: {
+       
+    },
+    h2Text:{
+
+    },
+    button:{
+
+    },
+
+});
